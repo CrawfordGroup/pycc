cce099fbb4c54c026be90612456a262fc0d66146
Aaron De Clercq <aaron.declercq@ugent.be>
2022-03-23 10:41:29 -0400

    Update test file

--- pycc/tests/test_020_cc2.py
+++ pycc/tests/test_020_cc2.py
@@ -3,6 +3,7 @@ Test CC2 equation solution using various molecule test cases.
 """
 
 # Import package, test suite, and other packages as needed
+from calendar import c
 import psi4
 import pycc
 import pytest
@@ -30,3 +31,9 @@ def test_cc2_h2o():
     ecc = cc.solve_cc(e_conv,r_conv,maxiter)
     epsi4 = -0.215857544656
     assert (abs(epsi4 - ecc) < 1e-11)
+
+    hbar = pycc.cchbar(cc)
+    cclambda = pycc.cclambda(cc, hbar)
+    lcc = cclambda.solve_lambda(e_conv, r_conv)
+    lcc_psi4 = -0.215765740373555
+    assert(abs(lcc - lcc_psi4) < 1e-11)
