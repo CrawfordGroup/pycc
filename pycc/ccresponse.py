305f27cfd70f7a49ce093a5afcfa852d5283ab45
T. Daniel Crawford <crawdad@vt.edu>
2022-05-30 11:58:49 +0200

    CCSD response properties

--- pycc/ccresponse.py
+++ pycc/ccresponse.py
@@ -6,7 +6,9 @@ if __name__ == "__main__":
     raise Exception("This file cannot be invoked on its own.")
 
 import numpy as np
-
+import time
+from .utils import helper_diis
+import opt_einsum
 
 class ccresponse(object):
     """
@@ -18,9 +20,7 @@ class ccresponse(object):
         Compute a CC linear response function.
     """
 
-    def __init__(self, ccdensity, 
-            omega1 = 0,
-            omega2 = 0):
+    def __init__(self, ccdensity, omega1 = 0, omega2 = 0):
         """
         Parameters
         ----------
@@ -42,23 +42,179 @@ class ccresponse(object):
         self.hbar = self.cclambda.hbar
         self.contract = self.ccwfn.contract
 
-        # Generate similarity-transformed property integrals
-        self.mubar = []
+        # Cartesian indices
+        self.cart = ["X", "Y", "Z"]
+
+        # Build dictionary of similarity-transformed property integrals
+        self.pertbar = {} # Empty dictionary
+
+        # Electric dipole operator
         for axis in range(3):
-            self.mubar.append(pertbar(self.H.mu[axis]))
+            key = "MU_" + self.cart[axis]
+            self.pertbar[key] = pertbar(self.H.mu[axis], self.ccwfn)
 
-        self.mbar = []
+        # Magnetic dipole operator
         for axis in range(3):
-            self.mbar.append(pertbar(self.H.m[axis]))
+            key = "M_" + self.cart[axis]
+            self.pertbar[key]  = pertbar(self.H.m[axis], self.ccwfn)
+
+        # HBAR-based denominators
+        eps_occ = np.diag(self.hbar.Hoo)
+        eps_vir = np.diag(self.hbar.Hvv)
+        self.Dia = eps_occ.reshape(-1,1) - eps_vir
+        self.Dijab = eps_occ.reshape(-1,1,1,1) + eps_occ.reshape(-1,1,1) - eps_vir.reshape(-1,1) - eps_vir
+
+    def linresp(self, A, B, omega):
+        A = A.upper()
+        B = B.upper()
+
+        # dictionaries for perturbed wave functions
+        X1 = {} 
+        X2 = {}
+        check = []
+        for axis in range(3):
+            pertkey = A + "_" + self.cart[axis]
+            X_key = pertkey + "_" + f"{omega:0.6f}"
+            print("Solving right-hand perturbed wave function for %s:" % (X_key))
+            X1[X_key], X2[X_key], polar = self.solve_right(self.pertbar[pertkey], omega)
+            check.append(polar)
+            if (omega != 0.0):
+                X_key = pertkey + "_" + f"{-omega:0.6f}"
+                print("Solving right-hand perturbed wave function for %s:" % (X_key))
+                X1[X_key], X2[X_key], polar = self.solve_right(self.pertbar[pertkey], -omega)
+                check.append(polar)
+
+        if (B != A):
+            for axis in range(3):
+                pertkey = B + "_" + self.cart[axis]
+                X_key = pertkey + "_" + f"{omega:0.6f}"
+                print("Solving right-hand perturbed wave function for %s:" % (X_key))
+                X1[X_key], X2[X_key], polar = self.solve_right(self.pertbar[pertkey], omega)
+                check.append(polar)
+                if (omega != 0.0):
+                    X_key = pertkey + "_" + f"{-omega:0.6f}"
+                    print("Solving right-hand perturbed wave function for %s:" % (X_key))
+                    X1[X_key], X2[X_key], polar = self.solve_right(self.pertbar[pertkey], -omega)
+                    check.append(polar)
+
+        return check
+
+    def solve_right(self, pertbar, omega, e_conv=1e-13, r_conv=1e-13, maxiter=200, max_diis=8, start_diis=1):
+        solver_start = time.time()
+
+        Dia = self.Dia
+        Dijab = self.Dijab
+
+        # initial guess
+        X1 = pertbar.Avo.T/(Dia + omega)
+        X2 = (pertbar.Avvoo+pertbar.Avvoo.swapaxes(0,1).swapaxes(2,3))/(Dijab + omega)
+
+        pseudo = self.pseudoresponse(pertbar, X1, X2)
+        print(f"Iter {0:3d}: CC Pseudoresponse = {pseudo:.15f}  dP = {-pseudo:.5E}")
 
 
-class pertbar(object):
-    def __init__(self, pert):
+        diis = helper_diis(X1, X2, max_diis)
+        contract = self.ccwfn.contract
+
+        self.X1 = X1
+        self.X2 = X2
+
+        for niter in range(1, maxiter+1):
+            pseudo_last = pseudo
+
+            X1 = self.X1
+            X2 = self.X2
+
+            r1 = self.r_X1(pertbar, omega)
+            r2 = self.r_X2(pertbar, omega)
+
+            self.X1 += r1/(Dia + omega)
+            self.X2 += r2/(Dijab + omega)
+
+            rms = contract('ia,ia->', np.conj(r1/(Dia+omega)), r1/(Dia+omega))
+            rms += contract('ijab,ijab->', np.conj(r2/(Dijab+omega)), r2/(Dijab+omega))
+            rms = np.sqrt(rms)
+
+            pseudo = self.pseudoresponse(pertbar, self.X1, self.X2)
+            pseudodiff = pseudo - pseudo_last
+            print(f"Iter {niter:3d}: CC Pseudoresponse = {pseudo:.15f}  dP = {-pseudo:.5E} rms = {rms:.5E}")
+
+            if ((abs(pseudodiff) < e_conv) and abs(rms) < r_conv):
+                print("\nPerturbed wave function converged in %.3f seconds.\n" % (time.time() - solver_start))
+                return self.X1, self.X2, pseudo
+
+            diis.add_error_vector(self.X1, self.X2)
+            if niter >= start_diis:
+                self.X1, self.X2 = diis.extrapolate(self.X1, self.X2)
+
+    def r_X1(self, pertbar, omega):
+        contract = self.contract
         o = self.ccwfn.o
         v = self.ccwfn.v
-        t1 = self.ccwfn.t1
+        X1 = self.X1
+        X2 = self.X2
+        hbar = self.hbar
+
+        r_X1 = (pertbar.Avo.T - omega * X1).copy()
+        r_X1 += contract('ie,ae->ia', X1, hbar.Hvv)
+        r_X1 -= contract('ma,mi->ia', X1, hbar.Hoo)
+        r_X1 += 2.0*contract('me,maei->ia', X1, hbar.Hovvo)
+        r_X1 -= contract('me,maie->ia', X1, hbar.Hovov)
+        r_X1 += contract('me,miea->ia', hbar.Hov, (2.0*X2 - X2.swapaxes(0,1)))
+        r_X1 += contract('imef,amef->ia', X2, (2.0*hbar.Hvovv - hbar.Hvovv.swapaxes(2,3)))
+        r_X1 -= contract('mnae,mnie->ia', X2, (2.0*hbar.Hooov - hbar.Hooov.swapaxes(0,1)))
+
+        return r_X1
+
+    def r_X2(self, pertbar, omega):
+        contract = self.contract
+        o = self.ccwfn.o
+        v = self.ccwfn.v
+        X1 = self.X1
+        X2 = self.X2
         t2 = self.ccwfn.t2
+        hbar = self.hbar
+        L = self.H.L
+
+        Zvv = contract('amef,mf->ae', (2.0*hbar.Hvovv - hbar.Hvovv.swapaxes(2,3)), X1)
+        Zvv -= contract('mnef,mnaf->ae', L[o,o,v,v], X2)
+
+        Zoo = -1.0*contract('mnie,ne->mi', (2.0*hbar.Hooov - hbar.Hooov.swapaxes(0,1)), X1)
+        Zoo -= contract('mnef,inef->mi', L[o,o,v,v], X2)
+
+        r_X2 = pertbar.Avvoo - 0.5 * omega*X2
+        r_X2 += contract('ie,abej->ijab', X1, hbar.Hvvvo)
+        r_X2 -= contract('ma,mbij->ijab', X1, hbar.Hovoo)
+        r_X2 += contract('mi,mjab->ijab', Zoo, t2)
+        r_X2 += contract('ae,ijeb->ijab', Zvv, t2)
+        r_X2 += contract('ijeb,ae->ijab', X2, hbar.Hvv)
+        r_X2 -= contract('mjab,mi->ijab', X2, hbar.Hoo)
+        r_X2 += 0.5*contract('mnab,mnij->ijab', X2, hbar.Hoooo)
+        r_X2 += 0.5*contract('ijef,abef->ijab', X2, hbar.Hvvvv)
+        r_X2 -= contract('imeb,maje->ijab', X2, hbar.Hovov)
+        r_X2 -= contract('imea,mbej->ijab', X2, hbar.Hovvo)
+        r_X2 += 2.0*contract('miea,mbej->ijab', X2, hbar.Hovvo)
+        r_X2 -= contract('miea,mbje->ijab', X2, hbar.Hovov)
+
+        r_X2 = r_X2 + r_X2.swapaxes(0,1).swapaxes(2,3)
+
+        return r_X2
+
+    def pseudoresponse(self, pertbar, X1, X2):
         contract = self.ccwfn.contract
+        polar1 = 2.0 * contract('ai,ia->', pertbar.Avo, X1)
+        polar2 = 2.0 * contract('ijab,ijab->', pertbar.Avvoo, (2.0*X2 - X2.swapaxes(2,3)))
+
+        return -2.0*(polar1 + polar2)
+        
+
+class pertbar(object):
+    def __init__(self, pert, ccwfn):
+        o = ccwfn.o
+        v = ccwfn.v
+        t1 = ccwfn.t1
+        t2 = ccwfn.t2
+        contract = opt_einsum.contract
 
         self.Aov = pert[o,v].copy()
 
@@ -66,17 +222,20 @@ class pertbar(object):
         self.Aoo += contract('ie,me->mi', t1, pert[o,v])
 
         self.Avv = pert[v,v].copy()
-        self.Avv -= contract('ma,me->ma', t1, pert[o,v])
+        self.Avv -= contract('ma,me->ae', t1, pert[o,v])
 
         self.Avo = pert[v,o].copy()
         self.Avo += contract('ie,ae->ai', t1, pert[v,v])
         self.Avo -= contract('ma,mi->ai', t1, pert[o,o])
-        self.Avo += contract('mieea,me->ai', (2.0*t2 - t2.swapaxis(2,3)), pert[o,v])
+        self.Avo += contract('miea,me->ai', (2.0*t2 - t2.swapaxes(2,3)), pert[o,v])
         self.Avo -= contract('ie,ma,me->ai', t1, t1, pert[o,v])
 
         self.Aovoo = contract('ijeb,me->mbij', t2, pert[o,v])
 
         self.Avvvo = -1.0*contract('miab,me->abei', t2, pert[o,v])
   
-        self.Avvoo = contract('ijeb,ae->abij', t2, Avv)
-        self.Avvoo -= contract('mjab,mi->abij', t2, Aoo)
+        # This is intended to match ugacc, but I need to re-derive all of these expressions for consistency
+        # Also note the incorrectness of my tensor notation for Avvoo
+        self.Avvoo = contract('ijeb,ae->ijab', t2, self.Avv)
+        self.Avvoo -= contract('mjab,mi->ijab', t2, self.Aoo)
+        # self.Avvoo = 0.5*(self.Avvoo + self.Avvoo.swapaxes(0,1).swapaxes(2,3))
--- pycc/tests/test_027_linresp.py
+++ pycc/tests/test_027_linresp.py
@@ -6,36 +6,39 @@ Test CCSD linear response functions.
 import psi4
 import pycc
 import pytest
-#from ..data.molecules import *
-import sys
-sys.path.insert(0, '/Users/crawdad/src/pycc/pycc/data')
-#import molecules as mol
-from molecules import *
+from ..data.molecules import *
 
 
-#def test_polar_h2o_cc_pvdz():
-"""H2O cc-pVDZ"""
-psi4.set_memory('2 GiB')
-psi4.core.set_output_file('output.dat', False)
-psi4.set_options({'basis': 'cc-pVDZ',
-                  'scf_type': 'pk',
-                  'mp2_type': 'conv',
-                  'freeze_core': 'false',
-                  'e_convergence': 1e-13,
-                  'd_convergence': 1e-13,
-                  'r_convergence': 1e-13,
-                  'diis': 1})
-mol = psi4.geometry(moldict["H2O"])
-rhf_e, rhf_wfn = psi4.energy('SCF', return_wfn=True)
+def test_polar_h2o_cc_pvdz():
+    """H2O cc-pVDZ"""
+    psi4.set_memory('2 GiB')
+    psi4.core.set_output_file('output.dat', False)
+    psi4.set_options({'basis': 'STO-3G',
+                      'scf_type': 'pk',
+                      'mp2_type': 'conv',
+                      'freeze_core': 'false',
+                      'e_convergence': 1e-14,
+                      'd_convergence': 1e-14,
+                      'r_convergence': 1e-14,
+                      'diis': 1})
+    mol = psi4.geometry(moldict["H2O"])
+    rhf_e, rhf_wfn = psi4.energy('SCF', return_wfn=True)
 
-e_conv = 1e-13
-r_conv = 1e-13
+    e_conv = 1e-13
+    r_conv = 1e-13
 
-cc = pycc.ccwfn(rhf_wfn)
-ecc = cc.solve_cc(e_conv, r_conv)
-hbar = pycc.cchbar(cc)
-cclambda = pycc.cclambda(cc, hbar)
-lecc = cclambda.solve_lambda(e_conv, r_conv)
-density = pycc.ccdensity(cc, cclambda)
+    cc = pycc.ccwfn(rhf_wfn)
+    ecc = cc.solve_cc(e_conv, r_conv)
+    hbar = pycc.cchbar(cc)
+    cclambda = pycc.cclambda(cc, hbar)
+    lecc = cclambda.solve_lambda(e_conv, r_conv)
+    density = pycc.ccdensity(cc, cclambda)
 
-resp = pycc.ccresponse(density)
+    resp = pycc.ccresponse(density)
+    omega = 0.01
+    check = resp.linresp("Mu", "Mu", omega)
+
+    ref = [0.059711553704, 0.056273457658, 7.341419446523, 7.129244769943, 3.071438076138, 2.989674229480]
+
+    for i in range(len(ref)):
+        assert(abs(check[i] - ref[i]) < 1e-12)
