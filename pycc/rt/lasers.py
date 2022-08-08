be2d17ba5d268141cf63a37def889f459aaea7c0
bgpeyton <bgpeyton@vt.edu>
2022-08-08 16:43:43 -0400

    Add tolerance to time check for pulse laser

--- pycc/rt/lasers.py
+++ pycc/rt/lasers.py
@@ -47,11 +47,12 @@ class sine_square_laser:
         return pulse
 
 class delta_pulse_laser:
-    def __init__(self, F_str, center=0.0):
+    def __init__(self, F_str, center=0.0, tol=1e-7):
         self.F_str = F_str
         self.center = center
+        self.tol = tol
     def __call__(self, t):
-        if t == self.center:
+        if abs(t - self.center) <= self.tol:
             pulse = self.F_str * 1.0
         else:
             pulse = 0
