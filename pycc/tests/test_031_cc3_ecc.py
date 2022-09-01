42ad26d2725f78c058b6e289450575320c103056
Zhe Wang <rozenberyl@gmail.com>
2022-09-01 11:12:41 -0400

    Adding another test with a bigger basis set.

--- pycc/tests/test_031_cc3_ecc.py
+++ pycc/tests/test_031_cc3_ecc.py
@@ -8,6 +8,7 @@ import pycc
 import pytest
 from ..data.molecules import *
 
+# H2O/STO-3G
 def test_cc3_h2o():
     # Psi4 Setup
     psi4.set_memory('2 GB')
@@ -31,7 +32,31 @@ def test_cc3_h2o():
     epsi4 = -0.07077804613624039
     assert (abs(epsi4 - ecc) < 1e-11)
 
-# cc-pVDZ 
+# H2/aug-cc-pVDZ
+def test_cc3_h2():
+    # Psi4 Setup
+    psi4.set_memory('2 GB')
+    psi4.core.set_output_file('output.dat', False)
+    psi4.set_options({'basis': 'aug-cc-pvdz',
+                      'scf_type': 'pk',
+                      'mp2_type': 'conv',
+                      'freeze_core': 'false',
+                      'e_convergence': 1e-12,
+                      'd_convergence': 1e-12,
+                      'r_convergence': 1e-12,
+                      'diis': 1})
+    mol = psi4.geometry(moldict["H2"])
+    rhf_e, rhf_wfn = psi4.energy('SCF', return_wfn=True)
+
+    maxiter = 75
+    e_conv = 1e-12
+    r_conv = 1e-12
+    cc = pycc.ccwfn(rhf_wfn, model='CC3')
+    ecc = cc.solve_cc(e_conv,r_conv,maxiter)
+    epsi4 = -0.03582003741348827
+    assert (abs(epsi4 - ecc) < 1e-11)
+
+# H2O/cc-pVDZ 
 """
 def test_cc3_h2o():
     # Psi4 Setup
