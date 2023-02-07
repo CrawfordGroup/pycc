8663b2e4dfe19b88c50c25f62af6fdab386474b3
Zhe Wang <rozenberyl@gmail.com>
2023-02-07 16:05:53 -0500

    Adding changes and test for single-precision and GPU option.

---
+++ pycc/tests/test_032_cc3_sp.py
@@ -0,0 +1,62 @@
+"""
+Test CC3 equation solution using various molecule test cases.
+"""
+
+# Import package, test suite, and other packages as needed
+import psi4
+import numpy as np
+import pycc
+import pytest
+from ..data.molecules import *
+
+# H2O/cc-pVDZ
+def test_cc3_h2o():
+    # Psi4 Setup
+    psi4.set_memory('2 GB')
+    psi4.core.set_output_file('output.dat', False)
+    psi4.set_options({'basis': 'cc-pVDZ',
+                      'scf_type': 'pk',
+                      'mp2_type': 'conv',
+                      'freeze_core': 'false',
+                      'e_convergence': 1e-12,
+                      'd_convergence': 1e-12,
+                      'r_convergence': 1e-12,
+                      'diis': 1})
+    mol = psi4.geometry(moldict["H2O_Teach"])
+    rhf_e, rhf_wfn = psi4.energy('SCF', return_wfn=True)
+
+    maxiter = 75
+    e_conv = 1e-7
+    r_conv = 1e-7
+    cc = pycc.ccwfn(rhf_wfn, model='CC3', precision='SP')
+    ecc = cc.solve_cc(e_conv,r_conv,maxiter)
+    epsi4 = -0.227888246840310
+    ecfour = -0.2278882468404231
+    assert (abs(epsi4 - ecc) < 1e-7)
+
+    hbar = pycc.cchbar(cc)
+    cclambda = pycc.cclambda(cc, hbar)
+    lcc = cclambda.solve_lambda(e_conv, r_conv)
+    print("lcc: ", lcc)
+    lcc_cfour = -0.2233231845185215 
+    assert(abs(lcc - lcc_cfour) < 1e-7)
+    
+    #ccdensity = pycc.ccdensity(cc, cclambda)
+    # no laser
+    #rtcc = pycc.rtcc(cc, cclambda, ccdensity, None, magnetic = False)
+    
+    # Nuclear dipole from Psi4: 1.12729111248 au
+    #nuc_dipole = mol.nuclear_dipole()
+    # Total dipole from CFOUR: [0, 0, 0.7703875967] au
+    """
+    ref = [0, 0, -0.3569035158] # au
+
+    mu_x, mu_y, mu_z = rtcc.dipole(cc.t1, cc.t2, cclambda.l1, cclambda.l2, withref=True)
+
+    assert (abs(ref[0] - np.real(mu_x)) < 1E-7)
+    assert (abs(ref[1] - np.real(mu_y)) < 1E-7)
+    assert (abs(ref[2] - np.real(mu_z)) < 1E-7)
+    """
+    #ccdensity = pycc.ccdensity(cc, cclambda)
+    #ecc = ccdensity.compute_energy()
+    #assert (abs(epsi4 - ecc) < 1e-11)
