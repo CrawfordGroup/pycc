706f300132d38f8cbfd8f9a8a966975c127794bd
Ben Peyton <bgpeyton@gmail.com>
2023-12-15 19:16:12 -0500

    remove python pin for now

--- setup.py
+++ setup.py
@@ -32,7 +32,6 @@ setup(
     version=versioneer.get_version(),
     cmdclass=versioneer.get_cmdclass(),
     license='BSD-3-Clause',
-    python_requires='<3.12',
     install_requires=[
         "numpy",
         "opt_einsum",
