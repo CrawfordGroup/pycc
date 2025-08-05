0bf1aae16a814b61e69b03b58d215a0167d93fbc
Jose Marc Madriaga <josemarcmadriaga@Joses-MacBook-Pro.local>
2025-08-05 10:55:45 -0400

    proper test for sim code QR

--- pycc/tests/test_038_sim_quadresp.py
+++ pycc/tests/test_038_sim_quadresp.py
@@ -12,17 +12,18 @@ def test_PNO_ccsd_SHG():
     """ H2O Second Harmonic Generation PNO-CCSD Test using local filter"""
     psi4.set_memory('2 GiB')
     psi4.core.set_output_file('output.dat', False)
-    psi4.set_options({'basis': 'cc-pvdz',
+    psi4.set_options({'basis': 'aug-cc-pvdz',
                       'scf_type': 'pk',
                       'freeze_core': 'false',
                       'e_convergence': 1e-12,
                       'd_convergence': 1e-12,
-                      'r_convergence': 1e-12})
-    mol = psi4.geometry(moldict["H2O"])
+                      'r_convergence': 1e-12,
+                      'local_maxiter': 10000})
+    mol = psi4.geometry(moldict["H2O_D"])
     rhf_e, rhf_wfn = psi4.energy('SCF', return_wfn=True)
     
-    e_conv = 1e-8
-    r_conv = 1e-8
+    e_conv = 1e-12
+    r_conv = 1e-12
     
     cc = pycc.ccwfn(rhf_wfn, local="PNO", local_cutoff=0, filter=True)
     ecc = cc.solve_cc(e_conv, r_conv)
@@ -67,7 +68,7 @@ def test_PNOpp_ccsd_OR():
     e_conv = 1e-12
     r_conv = 1e-12
     
-    cc = pycc.ccwfn(rhf_wfn, local="PNO++", local_cutoff = 1e-7, filter=True)
+    cc = pycc.ccwfn(rhf_wfn, local_mos='BOYS', local="PNO++", local_cutoff = 1e-7, filter=True)
     ecc = cc.solve_cc(e_conv, r_conv)
     hbar = pycc.cchbar(cc)
     cclambda = pycc.cclambda(cc, hbar)
@@ -93,42 +94,6 @@ def test_PNOpp_ccsd_OR():
     #HCl_OR = -19.34209144399                   #HCl = -19.342091493471
  
     #can only compare to itself
-    OR_sim = -14.631284641281
+    OR_sim = -25.787425247224956
     assert(abs(OR - OR_sim) < 1e-10)  
-
-def test_CPNOpp_ccsd_SHG():
-    """ H2O Second Harmonic Generation CPNO++-CCSD Test using local filter"""
-    psi4.set_memory('2 GiB')
-    psi4.core.set_output_file('output.dat', False)
-    psi4.set_options({'basis': 'aug-cc-pvdz',
-                      'scf_type': 'pk',
-                      'freeze_core': 'false',
-                      'e_convergence': 1e-12,
-                      'd_convergence': 1e-12,
-                      'r_convergence': 1e-12})
-    mol = psi4.geometry(moldict["H2O_D"])
-    rhf_e, rhf_wfn = psi4.energy('SCF', return_wfn=True)
-
-    e_conv = 1e-12
-    r_conv = 1e-12
-
-    #Testing different localization scheme for MOS -> Foster-Boys 
-    cc = pycc.ccwfn(rhf_wfn, local_mos = 'BOYS', local="CPNO++", local_cutoff = 1e-7, filter=True)
-    ecc = cc.solve_cc(e_conv, r_conv)
-    hbar = pycc.cchbar(cc)
-    cclambda = pycc.cclambda(cc, hbar)
-    lecc = cclambda.solve_lambda(e_conv, r_conv)
-    density = pycc.ccdensity(cc, cclambda)
-
-    resp = pycc.ccresponse(density)
-
-    #SHG frequencies
-    omega1 = 0.0656
-    omega2 = 0.0656
-
-    resp.pert_quadresp(omega1, omega2)
-    SHG = resp.hyperpolar()
-
-    #can only compare to itself
-    SHG_sim = -17.874897845906
-    assert(abs(SHG - SHG_sim) < 1e-10)   
+ 
